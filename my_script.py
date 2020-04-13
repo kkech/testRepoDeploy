f424a61f580372d44e59a812fc157366cc675582
Konstantinos <kostas24061992@yahoo.com>
2020-04-14 01:58:13 +0300

    test

--- my_script.py
+++ my_script.py
@@ -1,11 +1,23 @@
 
-from flask import Flask
+from flask import Flask, jsonify
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hey, we have Flask in a Docker container!'
+apps = [
+    {
+        'name':'test',
+        'gitUrl':'https://github.com/kkech/testRepoDeploy.git',
+        'version':'1.0'
+    },
+    {
+      'name':'test1',
+      'gitUrl':'https://github.com/kkech/testRepoDeploy.git',
+      'version':'1.1'
+    }
+]
 
+@app.route('/getVersion')
+def hello_world():
+    return jsonify({"apps": apps})
 
 if __name__=='__main__':
     app.run(debug=False, host='0.0.0.0')
